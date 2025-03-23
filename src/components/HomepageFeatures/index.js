d88176563a73e94d5a3c9537cb1b238082b4dcfa
Paul Deng <pauldeng93@gmail.com>
2025-03-22 23:57:03 -0700

    add missing lock file

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,11 +6,11 @@ export default function HomepageFeatures() {
     <section className={styles.introSection}>
       <div className="container">
         <div className="row">
-          <div className="col col--12">
+          
             <div className="text--center padding-horiz--md">
               {/* Removed MDXContent */}
+          
             </div>
-          </div>
         </div>
       </div>
     </section>
